478c8b4753a7ebbd09e13e96ed2b6693c27558f6
daniscodes <dan97@bell.net>
2022-08-17 23:11:29 -0400

    Implement character counter and refactor html section

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    
+  $("#tweet-text").on("input", function(e) {
+    let inputLength = $(".tweet-text").val()
+    $(this).siblings().children(".counter");
+    if (inputLength || inputLength === "") {
+      let  counter = 140 - inputLength.length;
+      $(this).siblings().children(".counter").val(counter);
+      if (counter < 0) {
+        $(this).siblings().children(".counter").css("color", "red");
+      } else {
+        $(this).siblings().children(".counter").css("color", "black");
+      } 
+    }
+  });
+})
