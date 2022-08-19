a03841792f9e69024fad422f403d993795bf574b
daniscodes <dan97@bell.net>
2022-08-19 07:33:11 -0400

    Remove html that i added to jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,18 +1,33 @@
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 const createTweetElement = function(data) {
-  return (`
+  let $tweet = $(`
   <article class="tweet">
   <header>
     <div class="user">
@@ -34,15 +49,15 @@ const createTweetElement = function(data) {
   </footer>
 </article>
   `);
+  return $tweet;
 };
 
-const $tweet = createTweetElement(tweetData)
-
+const renderTweet = function(data) {
+  for (let tweet of data) {
+    $('#tweets-bin').append(createTweetElement(tweet));
+  }
+}
 
 $(document).ready(function() {
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
+ renderTweet(data);
 }); 
