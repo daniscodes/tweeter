cd86a1e9b4fbdab6f6954c6101d5fa5a0659a24f
daniscodes <dan97@bell.net>
2022-08-19 08:21:46 -0400

    Fixing bugs and reformating ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,19 +1,38 @@
-// DOM using jQuery
-const createTweetElement = function (data) {
+$(document).ready(function () {
+
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  // DOM using jQuery
+const createTweetElement = function (tweet) {
+
   let $tweet = $(`
   <article class="tweet">
   <header>
     <div class="user">
       <img
-        src="${data.user.avatars}"
+        src="${tweet.user.avatars}"
         alt="">
-      <p>${data.user.name}</p>
+      <p>${tweet.user.name}</p>
     </div>
     <h4>${data.user.handle}</h4>
   </header>
-  <p>${data.content.text}</p>
+  <main>
+  <p>
+    ${escape(
+      tweet.content.text.length > 60
+        ? tweet.content.text.slice(0, 60) +
+            "\n" +
+            tweet.content.text.slice(60, tweet.content.text.length)
+        : tweet.content.text
+    )}
+  </p>
+</main>
   <footer>
-    <span>${data.created_at}</span>
+    <span>${timeago.format(tweet.created_at)}</span>
     <div>
       <i class="fas fa-flag"></i>
       <i class="fas fa-retweet"></i>
@@ -25,12 +44,14 @@ const createTweetElement = function (data) {
   return $tweet;
 };
 
-const renderTweet = function (data) {
-  for (let tweet of data) {
-    $('#tweets-bin').append(createTweetElement(tweet));
-  }
-}
+const renderTweets = function (arr) {
+  const $container = $(".tweet-bin");
+  $.each(arr, (key) => {
+    $container.prepend(createTweetElement(arr[key]));
+  });
 
+  return $container;
+};
 // AJAX
 const loadTweets = function () {
   $.ajax('/tweets', { method: 'GET' })
@@ -44,7 +65,6 @@ const loadTweets = function () {
 
 loadTweets()
 
-$(document).ready(function () {
 
   console.log('document is readyyy')
 
@@ -59,7 +79,7 @@ $(document).ready(function () {
         $('.tweet-text').val('')
       })
       .catch((err) => {
-        console.log('There was an error :(', err)
+        console.log('There was an error!!!!', err)
       })
   });
 
